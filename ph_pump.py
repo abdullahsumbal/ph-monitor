08110a82f58602332d0c7aae5ea63dbc9ebc358f
abdullahsumbal <abdullahsumbal@gmail.com>
2019-05-13 14:40:15 -0400

    change names

--- ph_pump.py
+++ ph_pump.py
@@ -6,47 +6,47 @@ changes state, all its dependents are notified and updatedautomatically.
 import abc
 
 
-class Subject:
+class Phmeter:
     """
-    Know its observers. Any number of Observer objects may observe a
-    subject.
-    Send a notification to its observers when its state changes.
+    Know its observers (pump). Any number of Observer objects (pumps) may observe a
+    phmeter value.
+    Send a notification to its observers(pumps) when ph state changes.
     """
 
     def __init__(self):
         self._observers = set()
-        self._subject_state = None
+        self._phmeter_state = None
 
     def attach(self, observer):
-        observer._subject = self
+        observer._phmeter = self
         self._observers.add(observer)
 
     def detach(self, observer):
-        observer._subject = None
+        observer._phmeter = None
         self._observers.discard(observer)
 
     def _notify(self):
         for observer in self._observers:
-            observer.update(self._subject_state)
+            observer.update(self._phmeter_state)
 
     @property
-    def subject_state(self):
-        return self._subject_state
+    def phmeter_state(self):
+        return self._phmeter_state
 
-    @subject_state.setter
-    def subject_state(self, arg):
-        self._subject_state = arg
+    @phmeter_state.setter
+    def phmeter_state(self, arg):
+        self._phmeter_state = arg
         self._notify()
 
 
 class Observer(metaclass=abc.ABCMeta):
     """
     Define an updating interface for objects that should be notified of
-    changes in a subject.
+    changes in a phmeter.
     """
 
     def __init__(self):
-        self._subject = None
+        self._phmeter = None
         self._observer_state = None
 
     @abc.abstractmethod
@@ -57,8 +57,8 @@ class Observer(metaclass=abc.ABCMeta):
 class ConcreteObserver(Observer):
     """
     Implement the Observer updating interface to keep its state
-    consistent with the subject's.
-    Store state that should stay consistent with the subject's.
+    consistent with the phmeter's.
+    Store state that should stay consistent with the phmeter's.
     """
 
     def update(self, arg):
@@ -67,10 +67,10 @@ class ConcreteObserver(Observer):
 
 
 def main():
-    subject = Subject()
+    phmeter = Phmeter()
     concrete_observer = ConcreteObserver()
-    subject.attach(concrete_observer)
-    subject.subject_state = 123
+    phmeter.attach(concrete_observer)
+    phmeter.phmeter_state = 123
 
 
 if __name__ == "__main__":
