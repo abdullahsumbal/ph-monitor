5ce1a799e982e37bc11cc58c5ccf5e2c60f84d1b
abdullahsumbal <abdullahsumbal@gmail.com>
2019-05-13 16:53:47 -0400

    fixed pump console code for python3

--- pump.py
+++ pump.py
@@ -22,7 +22,23 @@ def connectPump(ports):
     return serialConnections
 
 
+def sendCommand(ser, command, waitForOutput=True):
+    # encode string
+    commandEncoded = command.encode()
+    # write
+    ser.write(commandEncoded)
+    # wait for the output to return
+    if waitForOutput:
+        time.sleep(0.1)
+        output = ''
+        while ser.inWaiting() > 0:
+            output += ser.read(1).decode()
+        if output != '':
+            return output
+    return None
+
 def serialConsole(ser):
+    #ser.open()
     print('Enter your commands below.\r\nInsert "exit" to leave the application.')
     userInput = 1
     while 1:
@@ -31,24 +47,32 @@ def serialConsole(ser):
         # input = raw_input(">> ")
         # Python 3 users
         userInput = input(">> ")
+
+        # type exit to exit console
         if userInput == 'exit':
             ser.close()
             exit()
         else:
             # send the character to the device
             # (note that I dont happend a \r\n carriage return and line feed to the characters - this is not requested by my device)
-            ser.write(userInput)
+
+            #convert to bytes
+            userInputBytes = userInput.encode()
+            ser.write(userInputBytes)
             out = ''
             # let's wait one second before reading output (let's give device time to answer)
             time.sleep(1)
             while ser.inWaiting() > 0:
-                out += ser.read(1)
+                out += ser.read(1).decode()
 
             if out != '':
-                print (">> " + out)
+                print(">> " + out)
 
 
 if __name__ == '__main__':
-    serialConnections = connectPump(['COM4'])
+    serialConnections = connectPump(['COM3'])
     ser = serialConnections[0]
+    # send one command
+    print(sendCommand(ser, "DSP?", waitForOutput=True))
+    # start console
     serialConsole(ser)
