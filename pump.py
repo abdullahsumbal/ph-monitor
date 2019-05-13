7b891dbaa6f471225e4222072ed4433e4e6ded67
abdullahsumbal <abdullahsumbal@gmail.com>
2019-05-13 16:21:31 -0400

    refactoring pump code

--- pump.py
+++ pump.py
@@ -1,39 +1,54 @@
 import time
 import serial
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-    port='COM6',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
-)
 
-print(ser)
+def connectPump(ports):
 
-ser.isOpen()
+    serialConnections = []
 
-print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
+    for port in ports:
 
-input=1
-while 1 :
-    # get keyboard input
-    input = raw_input(">> ")
+        # configure the serial connections (the parameters differs on the device you are connecting to)
+        ser = serial.Serial(
+            port=port,
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS
+        )
+        ser.isOpen()
+        serialConnections.append(ser)
+
+    return serialConnections
+
+
+def serialConsole(ser):
+    print('Enter your commands below.\r\nInsert "exit" to leave the application.')
+    userInput = 1
+    while 1:
+        # get keyboard input
+        # Python 2 users
+        # input = raw_input(">> ")
         # Python 3 users
-        # input = input(">> ")
-    if input == 'exit':
-        ser.close()
-        exit()
-    else:
-        # send the character to the device
-        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-        ser.write(input)
-        out = ''
-        # let's wait one second before reading output (let's give device time to answer)
-        time.sleep(1)
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
+        userInput = input(">> ")
+        if userInput == 'exit':
+            ser.close()
+            exit()
+        else:
+            # send the character to the device
+            # (note that I dont happend a \r\n carriage return and line feed to the characters - this is not requested by my device)
+            ser.write(userInput)
+            out = ''
+            # let's wait one second before reading output (let's give device time to answer)
+            time.sleep(1)
+            while ser.inWaiting() > 0:
+                out += ser.read(1)
 
-        if out != '':
-            print ">> " + out
+            if out != '':
+                print (">> " + out)
+
+
+if __name__ == '__main__':
+    serialConnections = connectPump(['COM4'])
+    ser = serialConnections[0]
+    serialConsole(ser)
